379060baeb7930eb6761ae6b650957019ba12371
DESKTOP-AUA2OTD\hugoh <hugohenriques98@gmail.com>
2021-06-16 12:37:12 +0100

    ---

--- chat.php
+++ chat.php
@@ -4,6 +4,6 @@ require './vendor/autoload.php';
 
 use Chat\ChatServer;
 
-$app = new Ratchet\App('project-akrasia-websockets.herokuapp.com', 9990);
+$app = new Ratchet\App('project-akrasia-websockets.herokuapp.com', 3000);
 $app->route('/chat', new ChatServer, ['*']);
 $app->run();
