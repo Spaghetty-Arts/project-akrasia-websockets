f2f679b7f61650b7cf24a6d6e8f2f42d998caa59
Fabian Nunes <54534495+fabian-nunes@users.noreply.github.com>
2021-06-23 15:39:28 +0100

    Update chat.php

--- chat.php
+++ chat.php
@@ -4,6 +4,6 @@ require './vendor/autoload.php';
 
 use Chat\ChatServer;
 
-$app = new Ratchet\App('https://vigorous-shirley-5921e5.netlify.app', 3000);
+$app = new Ratchet\App('localhost', 3000);
 $app->route('/chat', new ChatServer, ['*']);
 $app->run();
